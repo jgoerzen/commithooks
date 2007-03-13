860d5b5175aa42341c7334d38a396a4e89d7809e
John Goerzen <jgoerzen@complete.org>
2007-03-13 13:37:29 -0500

    Initial add from /usr/local/bin

---
+++ commit-messages-split.sh
@@ -0,0 +1,80 @@
+#! /bin/bash
+
+# usage:
+# $1 mail address
+
+# Originally written by Andres Loeh
+# Changed by Duncan Coutts to send an email per-patch rather than per-push
+# Modified by John Goerzen to use trac-post-commit-hook
+
+#email=$1
+
+[[ -z ${DARCS} ]] && DARCS="/usr/bin/darcs"
+[[ -z ${CURRENTHASH} ]] && CURRENTHASH="_darcs/current-hash"
+
+hash=$(cat ${CURRENTHASH})
+
+# find all the patches since the one identified by the current hash
+patches=$(${DARCS} changes		\
+	--reverse			\
+	--from-match="hash ${hash}"	\
+	--match="not hash ${hash}"	\
+	--xml-output			\
+	| grep "hash='"			\
+	| sed "s/^.*hash='\([^']*\)'.*$/\1/")
+
+# update the current hash to the last hash we've seen.
+patch=${patches##*[${IFS}[:cntrl:]]}
+if [[ -n ${patch} ]]; then
+	echo ${patch} > ${CURRENTHASH}
+fi
+
+# Send the emails asynchronously so we don't hold up the apply process
+# This also allows the sending of the emails to take it's time and do
+# them in the proper chronological order.
+#(
+	# send an email for each patch
+	for patch in ${patches}; do
+	
+	        # sleep for at least a second otherwise all the emails sent can have
+		# the same timestamp and we loose the order of the patches.
+		sleep 1.1
+		
+		patchname=$(${DARCS} changes		\
+			--matches="hash ${patch}"	\
+			--xml-output			\
+			| grep '<name>'			\
+			| sed 's|.*<name>\(.*\)</name>.*|\1|')
+
+                author=$(${DARCS} changes               \
+                        --matches="hash ${patch}"       \
+                        --quiet                         \
+                        | grep '^[A-Z]'                 \
+                        | sed 's/^.* //'           )
+
+                rev=$(${DARCS} changes                  \
+                       --to-match="hash ${patch}"       \
+                       --quiet                          \
+                       | grep '^[A-Z]'                  \
+                       | wc -l                     )
+
+                echo "Scanning rev ${rev} for ticket tags..."
+
+                project=$(basename $(pwd))
+
+                log=$(${DARCS} changes                  \
+                       --matches="hash ${patch}"        )
+
+                /usr/local/bin/trac-post-commit-hook    \
+                       -p "/home/trac/instances/${project}" \
+                       -r "${rev}" \
+                       -u "${author}" \
+                       -s "http://software.complete.org/${project}" \
+                       -m "${log}" \
+                       || true
+		
+	done
+#) &
+
+exit 0
+
