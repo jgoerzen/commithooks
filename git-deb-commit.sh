1834de170ade863b9648f41bdb689fdc48469ab2
John Goerzen <jgoerzen@complete.org>
2008-02-26 14:52:19 -0600

    Make git-deb-commit.sh work with git (initial run)

--- git-deb-commit.sh
+++ git-deb-commit.sh
@@ -2,7 +2,17 @@
 
 # Usage:
 #
-#  $1 Path to Mercurial web server for this repo
+#  $1 Path to gitweb web server for this repo
+#     
+#     Should end with '?' if pathinfo is used or ';' otherwise
+#
+#     Examples:
+#     
+#     http://git.kernel.org/gitweb.cgi?p=boot/syslinux/syslinux-gpxe.git;
+#     http://repo.or.cz/w/git.git?
+#
+#     After this, we can append things like "a=shortlog"
+#
 #  $2 From address for mails to Debian BTS
 set -e
 
@@ -11,20 +21,26 @@ FROMADDR="$2"
 
 CMD="$(dirname $(readlink -f $0))/deb-post-commit-hook"
 
-FILE="`mktemp -t hg-commit.sh.XXXXXXXXXX`"
+FILE="`mktemp -t git-commit.sh.XXXXXXXXXX`"
 
-hg log -v -r "$HG_NODE" | grep -v "^files:" > "$FILE"
+while read gitrev; do
 
+git show -s --pretty "$gitrev" > "$FILE"
 cat >>"$FILE" <<EOF
 
-Diff: $URL?cmd=changeset;node=$HG_NODE;style=raw
+Diff: ${URL}a=commitdiff_plain;h=${gitrev}"
+
 EOF
 
+git diff-tree --stat --summary --find-copies-harder \
+    "${gitrev}^..${gitrev}" >> "$FILE"
+
+
 "$CMD" -r "$HG_NODE" \
-    -u "`hg log -r $HG_NODE | grep '^user:' | sed 's/^user: *//'`" \
-    -m "`hg log -v -r $HG_NODE | sed '1,/^description:/ d'`" \
+    -u "git show -s --pretty=format:%an ${gitrev}" \
+    -m "git show -s --pretty=format:%s%n%b ${gitrev}" \
     -s "bugs.debian.org" \
-    -U "$URL?cmd=changeset;node=$HG_NODE;style=gitweb" \
+    -U "${URL}a=commit;h=${gitrev}" \
     -f "$FILE" \
     -F "$2"
 
